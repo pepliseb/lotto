3f525fd1688779fa44b7d99ed8b014500d624afc
pepliseb <eman@onet.eu>
2018-03-18 16:49:37 +0100

    Initial commit

---
+++ src/main/java/pl/pepliseb/lotto/model/Lotto.java
@@ -0,0 +1,15 @@
+package pl.pepliseb.lotto.model;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Lotto {
+    static List<Integer> generateLottoNumber(){
+        Random random = new Random();
+        random.nextInt(49);
+        Set<Integer>  number = new TreeSet<>();
+        number.add()
+    }
+}
